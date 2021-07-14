34f737b3980dc92eedb1481fc0679889137e1d7e
ali sheykhiyan <80154787+alisheykhiyan@users.noreply.github.com>
2021-07-14 11:52:23 +0430

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -1,5 +1,7 @@
 # phonebook.py
-print ("Type number '1' to add a contact to the list,type number '2' to see the contacts and the numbers ,try number '3' if you want to do the both reading and appending or press 'Enter' key to exit.")
+print ("Type number '1' to add a contact to the list,",
+       "type number '2' to see the contacts and the numbers ,",
+       "try number '3' if you want to do the both reading and appending or press 'Enter' key to exit.")
 choosen_command = input (":")
 while choosen_command != "Enter":
    if choosen_command == '1':
