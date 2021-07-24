1ee422e258a85cf52336464568cfbf4ea9e0315b
ali sheykhiyan <80154787+alisheykhiyan@users.noreply.github.com>
2021-07-24 21:46:57 +0430

    Update phonebook.py
    salam amo.
    babat takhir ozr mikham bahone nist vali madrese kelas o film gozash in hafte yekam saram shologh shod ye meghdari ham yadam rafte bod =) kholase dige alan amade am madrese ham do se roz bakhater corona tatile .
    dar mored kar inke mn to line haftom eshteb zadam na? in bahs ro daram mikhonam o search mizanam vali bizahmat ye tozih ham age mishe bedi :)bad inke kam kam chon khodet gofti kam code zadam

--- phonebook.py
+++ phonebook.py
@@ -1,37 +1,10 @@
 # phonebook.py
 print ("Type number '1' to add a contact to the list,",
        "type number '2' to see the contacts and the numbers ,",
-       "try number '3' if you want to do the both reading and appending or press 'Enter' key to exit.")
+       "try number '3' if you want to do the both reading and ",
+       "appending or press 'Enter' key to exit.")
 choosen_command = input (":")
 while choosen_command != "Enter":
-   if choosen_command == '1':
-       contactname = input ("Enter the contacts name:")
-       contactnum = int(input("Enter the contacts number:"))
-       p = open ('phonebook.txt','w')
-       # ("pp") equals to phonebooks_parts .
-       pp = (contactname,":",contactnum) 
-       p.write(pp)
-       p.close()
-   if choosen_command == '2' :
-       p = open ('phonebook.txt','r') 
-       print(p.read())
-       p.close()
-   if choosen_command == '3' :
-       decision = input ("Do you mind reading or appending fisrt?(r/a)")
-       if decision == 'r' :
-           p = open ('phonebook.txt','w+')
-           print (p.read())
-           contactname = input ("Enter the contacts name:")
-           contactnum = int(input("Enter the contacts number:"))
-           p.write(contactname,"=",contactnum)
-           p.close() 
-       if decision == 'a' :
-           p = open ('phonebook.txt','w+')
-           contactname = input ("Enter the contacts name:")
-           contactnum = int(input("Enter the contacts number:"))
-           p.write(contactname,"=",contactnum) 
-           print (p.read())
-           p.close()
  
        
            
