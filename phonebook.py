40a4bc60e1f83248121bf3c5e733a4cea5693780
ali sheykhiyan <80154787+alisheykhiyan@users.noreply.github.com>
2021-09-18 18:05:45 +0430

    Filling the main branch

--- phonebook.py
+++ phonebook.py
@@ -1,1 +1,24 @@
+# phonebook.py
+print ("------------------------------ welcom to phonebook ------------------------------")
+
+# Hinting to user the directions to use the program:
+
+print ("Enter number 1 to add a number or 2 to quit the program:")
+
+while True :
+    decision =int(input("Enter the number 1 or 2 to continue :"))
+
+    # If user needs to add number(enters '1'):
+    if decision == 1:
+        name = input("Please enter the contacts name :")
+        number = int(input("Then please enter number belongs to this contact :"))
+
+    # If user needs to close program(enters '2'):
+    elif decision == 2:
+        break
+
+    # If user enters inappropriat input:
+    else:
+        print("Sorry,inappropriat number or letter entered .")
+        
 
