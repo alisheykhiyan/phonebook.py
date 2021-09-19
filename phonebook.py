1fab5208daea09375ff0ee8c391635c1920e1e3d
ali sheykhiyan <80154787+alisheykhiyan@users.noreply.github.com>
2021-09-19 21:36:00 +0430

    adding codes to main.

--- phonebook.py
+++ phonebook.py
@@ -1,1 +1,26 @@
+# phonebook.py
+print ("------------------------------ welcom to phonebook ------------------------------")
+
+# Hinting to user the directions to use the program:
+
+print ("Enter number 1 to add a number or 2 to quit the program:")
+
+while True :
+    run_command =int(input("Enter the number (1/2) :"))
+
+    # If user needs to add number(enters '1'):
+    if decision == 1:
+        contact_name = input("Please enter the contacts name :")
+        contact_number = int(input("Then please enter number belongs to this contact :"))
+
+    # If user needs to close program(enters '2'):
+    elif decision == 2:
+        break
+
+    # If user enters inappropriat input:
+    else:
+        print("Sorry,inappropriat number or letter entered .")
+        
+    
+
 
