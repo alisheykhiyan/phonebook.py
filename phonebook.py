ed238ea540e08eaea05f215a381e596a936c1ec4
ali sheykhiyan <80154787+alisheykhiyan@users.noreply.github.com>
2021-11-20 19:01:24 +0330

    Inserting the base code.

--- phonebook.py
+++ phonebook.py
@@ -1,1 +1,44 @@
+# phonebook.py
+
+
+print ("--------- welcom to phonebook ---------")
+
+
+
+# Hinting to user the directions to use the program :
+
+
+
+print ("Enter number 1 to add a number or 2 to quit the program:")
+
+
+
+# Reciving the users preferation :
+while True :
+    run_decision = int(input("Enter the number 1 or 2 to continue :"))
+
+
+
+
+    # What if user preffered (1):
+    if run_decision == 1:
+        contact_name = input("Please enter the contacts name :")
+        contact_number = int(input("Then please enter number belongs",
+                                   "to this contact :"))
+
+
+
+
+    # What if user preffered (2):
+    elif run_decision == 2:
+        break
+
+
+
+
+    # What if user enters inappropriat input:
+    else:
+        print("Sorry,inappropriat number or letter entered .")
+        
+    
 
