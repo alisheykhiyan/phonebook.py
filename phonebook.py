3afef7643cea652bf9a7d65e0af35943254b4368
ali sheykhiyan <80154787+alisheykhiyan@users.noreply.github.com>
2023-12-29 22:16:43 +0330

    Initializing basic phonebook.

--- phonebook.py
+++ phonebook.py
@@ -1,1 +1,25 @@
+# Phonebook.py
+print("---------------Welcome to phonebook---------------")
+command = int(input("To add a contact enter number 1 and to exit, print number 0 : "))
+if command == 0:
+    print("Thanks for running the program !")
+    exit()
+elif command == 1:
+    name = input("Enter the contacts name : ")
+    num = int(input("Enter the contacts numer : "))
+else:
+    print("ERROR: Invalid input")
 
+while True:
+    command = int(input("To see the contacts name and number press 1, to add a new contact press 2,"
+                        "and press 0 to quit the program :"))
+    if command == 0:
+        print("Thanks for running the program !")
+        break
+    elif command == 1:
+        print(f"Name: {name} ", f"Number: {num}")
+    elif command == 2:
+        name = input("Enter the contacts name : ")
+        num = int(input("Enter the contacts numer : "))
+    else:
+        print("ERROR: Invalid input")
